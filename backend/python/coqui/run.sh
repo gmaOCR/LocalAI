25b5527f82c8c814053f434acd5d9d81fee52647
Ettore Di Giacinto <mudler@users.noreply.github.com>
2023-12-24 13:38:54 -0500

    feat: add 🐸 coqui (#1489)
    * feat: add coqui
    
    * docs: update news

--- backend/python/coqui/run.sh
+++ backend/python/coqui/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-backend_dir=$(dirname $0)
-if [ -d $backend_dir/common ]; then
-    source $backend_dir/common/libbackend.sh
-else
-    source $backend_dir/../common/libbackend.sh
-fi
 
-startBackend $@
+##
+## A bash script wrapper that runs the ttsbark server with conda
+
+export PATH=$PATH:/opt/conda/bin
+
+# Activate conda environment
+source activate transformers
+
+# get the directory where the bash script is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+python $DIR/coqui_server.py $@
