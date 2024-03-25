3789d0f4781363a240727e5c765bfe20f4fe86ed
Ettore Di Giacinto <mudler@users.noreply.github.com>
2024-03-25 22:04:32 +0100

    fix(aio): make image-gen for GPU functional, update docs (#1895)
    * readme: update quickstart
    
    * aio(gpu): fix dreamshaper
    
    * tests(aio): allow to run tests also against an endpoint
    
    * docs: split content
    
    * tests: less verbosity
    
    ---------
    
    Co-authored-by: Dave <dave@gray101.com>

--- tests/e2e-aio/e2e_suite_test.go
+++ tests/e2e-aio/e2e_suite_test.go
@@ -7,17 +7,6 @@ import (
 	"runtime"
 	"testing"
 
-<<<<<<< HEAD
-	"github.com/docker/go-connections/nat"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"github.com/sashabaranov/go-openai"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
-)
-
-var container testcontainers.Container
-=======
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/ory/dockertest/v3"
@@ -27,22 +16,13 @@ var container testcontainers.Container
 
 var pool *dockertest.Pool
 var resource *dockertest.Resource
->>>>>>> f1b4a748 (feat(aio): add tests, update model definitions (#1880))
 var client *openai.Client
 
 var containerImage = os.Getenv("LOCALAI_IMAGE")
 var containerImageTag = os.Getenv("LOCALAI_IMAGE_TAG")
 var modelsDir = os.Getenv("LOCALAI_MODELS_DIR")
-<<<<<<< HEAD
-var apiEndpoint = os.Getenv("LOCALAI_API_ENDPOINT")
-var apiKey = os.Getenv("LOCALAI_API_KEY")
-
-const (
-	defaultApiPort = "8080"
-)
-=======
 var apiPort = os.Getenv("LOCALAI_API_PORT")
->>>>>>> f1b4a748 (feat(aio): add tests, update model definitions (#1880))
+var apiEndpoint = os.Getenv("LOCALAI_API_ENDPOINT")
 
 func TestLocalAI(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -51,19 +31,18 @@ func TestLocalAI(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 
-<<<<<<< HEAD
+	if apiPort == "" {
+		apiPort = "8080"
+	}
+
 	var defaultConfig openai.ClientConfig
 	if apiEndpoint == "" {
 		startDockerImage()
-		apiPort, err := container.MappedPort(context.Background(), nat.Port(defaultApiPort))
-		Expect(err).To(Not(HaveOccurred()))
-
-		defaultConfig = openai.DefaultConfig(apiKey)
-		apiEndpoint = "http://localhost:" + apiPort.Port() + "/v1" // So that other tests can reference this value safely.
-		defaultConfig.BaseURL = apiEndpoint
+		defaultConfig = openai.DefaultConfig("")
+		defaultConfig.BaseURL = "http://localhost:" + apiPort + "/v1"
 	} else {
-		GinkgoWriter.Printf("docker apiEndpoint set from env: %q\n", apiEndpoint)
-		defaultConfig = openai.DefaultConfig(apiKey)
+		fmt.Println("Default ", apiEndpoint)
+		defaultConfig = openai.DefaultConfig("")
 		defaultConfig.BaseURL = apiEndpoint
 	}
 
@@ -73,45 +52,30 @@ var _ = BeforeSuite(func() {
 	Eventually(func() error {
 		_, err := client.ListModels(context.TODO())
 		return err
-	}, "50m").ShouldNot(HaveOccurred())
+	}, "20m").ShouldNot(HaveOccurred())
 })
 
 var _ = AfterSuite(func() {
-	if container != nil {
-		Expect(container.Terminate(context.Background())).To(Succeed())
+	if resource != nil {
+		Expect(pool.Purge(resource)).To(Succeed())
 	}
+	//dat, err := os.ReadFile(resource.Container.LogPath)
+	//Expect(err).To(Not(HaveOccurred()))
+	//Expect(string(dat)).To(ContainSubstring("GRPC Service Ready"))
+	//fmt.Println(string(dat))
 })
 
 var _ = AfterEach(func() {
-	// Add any cleanup needed after each test
+	//Expect(dbClient.Clear()).To(Succeed())
 })
 
-type logConsumer struct {
-}
-
-func (l *logConsumer) Accept(log testcontainers.Log) {
-	GinkgoWriter.Write([]byte(log.Content))
-}
-
 func startDockerImage() {
-=======
-	if containerImage == "" {
-		Fail("LOCALAI_IMAGE is not set")
-	}
-	if containerImageTag == "" {
-		Fail("LOCALAI_IMAGE_TAG is not set")
-	}
-	if apiPort == "" {
-		apiPort = "8080"
-	}
-
 	p, err := dockertest.NewPool("")
 	Expect(err).To(Not(HaveOccurred()))
 	Expect(p.Client.Ping()).To(Succeed())
 
 	pool = p
 
->>>>>>> f1b4a748 (feat(aio): add tests, update model definitions (#1880))
 	// get cwd
 	cwd, err := os.Getwd()
 	Expect(err).To(Not(HaveOccurred()))
@@ -122,48 +86,6 @@ func startDockerImage() {
 	}
 
 	proc := runtime.NumCPU()
-<<<<<<< HEAD
-
-	req := testcontainers.ContainerRequest{
-
-		Image:        fmt.Sprintf("%s:%s", containerImage, containerImageTag),
-		ExposedPorts: []string{defaultApiPort},
-		LogConsumerCfg: &testcontainers.LogConsumerConfig{
-			Consumers: []testcontainers.LogConsumer{
-				&logConsumer{},
-			},
-		},
-		Env: map[string]string{
-			"MODELS_PATH":                   "/models",
-			"DEBUG":                         "true",
-			"THREADS":                       fmt.Sprint(proc),
-			"LOCALAI_SINGLE_ACTIVE_BACKEND": "true",
-		},
-		Files: []testcontainers.ContainerFile{
-			{
-				HostFilePath:      md,
-				ContainerFilePath: "/models",
-				FileMode:          0o755,
-			},
-		},
-		WaitingFor: wait.ForAll(
-			wait.ForListeningPort(nat.Port(defaultApiPort)),
-		//	wait.ForHTTP("/v1/models").WithPort(nat.Port(apiPort)).WithStartupTimeout(50*time.Minute),
-		),
-	}
-
-	GinkgoWriter.Printf("Launching Docker Container %s:%s\n", containerImage, containerImageTag)
-
-	ctx := context.Background()
-	c, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	Expect(err).To(Not(HaveOccurred()))
-
-	container = c
-}
-=======
 	options := &dockertest.RunOptions{
 		Repository: containerImage,
 		Tag:        containerImageTag,
@@ -179,28 +101,4 @@ func startDockerImage() {
 	Expect(err).To(Not(HaveOccurred()))
 
 	resource = r
-
-	defaultConfig := openai.DefaultConfig("")
-	defaultConfig.BaseURL = "http://localhost:" + apiPort + "/v1"
-
-	// Wait for API to be ready
-	client = openai.NewClientWithConfig(defaultConfig)
-
-	Eventually(func() error {
-		_, err := client.ListModels(context.TODO())
-		return err
-	}, "20m").ShouldNot(HaveOccurred())
-})
-
-var _ = AfterSuite(func() {
-	Expect(pool.Purge(resource)).To(Succeed())
-	//dat, err := os.ReadFile(resource.Container.LogPath)
-	//Expect(err).To(Not(HaveOccurred()))
-	//Expect(string(dat)).To(ContainSubstring("GRPC Service Ready"))
-	//fmt.Println(string(dat))
-})
-
-var _ = AfterEach(func() {
-	//Expect(dbClient.Clear()).To(Succeed())
-})
->>>>>>> f1b4a748 (feat(aio): add tests, update model definitions (#1880))
+}
