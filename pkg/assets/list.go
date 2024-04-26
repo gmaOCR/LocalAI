e33f2aefb2dd981e78756307f5d9dc1976715fdb
Dave <dave@gray101.com>
2024-04-26 04:34:31 -0400

    fix: security scanner warning noise: error handlers part 1 (#2141)
    first group of error handlers to reduce security scanner warning noise level
    
    Signed-off-by: Dave Lee <dave@gray101.com>

--- pkg/assets/list.go
+++ pkg/assets/list.go
@@ -1,43 +1,27 @@
 package assets
 
 import (
-<<<<<<< HEAD
-	"os"
+	"embed"
+	"io/fs"
 
-	rice "github.com/GeertJohan/go.rice"
 	"github.com/rs/zerolog/log"
 )
 
-func ListFiles(content *rice.Box) (files []string) {
-	err := content.Walk("", func(path string, info os.FileInfo, err error) error {
-=======
-	"embed"
-	"io/fs"
-)
-
 func ListFiles(content embed.FS) (files []string) {
-	fs.WalkDir(content, ".", func(path string, d fs.DirEntry, err error) error {
->>>>>>> fda6bf56 (feat: embedded model configurations, add popular model examples, refactoring (#1532))
+	err := fs.WalkDir(content, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-<<<<<<< HEAD
-		if info.IsDir() {
-=======
 		if d.IsDir() {
->>>>>>> fda6bf56 (feat: embedded model configurations, add popular model examples, refactoring (#1532))
 			return nil
 		}
 
 		files = append(files, path)
 		return nil
 	})
-<<<<<<< HEAD
 	if err != nil {
-		log.Error().Err(err).Msg("error walking the rice box")
+		log.Error().Err(err).Msg("error walking the embedded filesystem")
 	}
-=======
->>>>>>> fda6bf56 (feat: embedded model configurations, add popular model examples, refactoring (#1532))
 	return
 }
