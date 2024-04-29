aca1bca4ed41f4a8bdc94390e0083507206b28db
Dave <dave@gray101.com>
2024-04-29 13:42:37 -0400

    refactor(application): introduce application global state (#2072)
    * start breaking up the giant channel refactor now that it's better understood - easier to merge bites
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * add concurrency and base64 back in, along with new base64 tests.
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * Automatic rename of whisper.go's Result to TranscriptResult
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * remove pkg/concurrency - significant changes coming in split 2
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * fix comments
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * add list_model service as another low-risk service to get it out of the way
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * split backend config loader into seperate file from the actual config struct. No changes yet, just reduce cognative load with smaller files of logical blocks
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * rename state.go ==> application.go
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    * fix lost import?
    
    Signed-off-by: Dave Lee <dave@gray101.com>
    
    ---------
    
    Signed-off-by: Dave Lee <dave@gray101.com>

--- core/backend/transcript.go
+++ core/backend/transcript.go
@@ -67,7 +67,7 @@ func ModelTranscription(audio, language string, translate bool, ml *model.ModelL
 	model "github.com/go-skynet/LocalAI/pkg/model"
 )
 
-func ModelTranscription(audio, language string, ml *model.ModelLoader, backendConfig config.BackendConfig, appConfig *config.ApplicationConfig) (*schema.Result, error) {
+func ModelTranscription(audio, language string, ml *model.ModelLoader, backendConfig config.BackendConfig, appConfig *config.ApplicationConfig) (*schema.TranscriptionResult, error) {
 
 	opts := modelOpts(backendConfig, appConfig, []model.Option{
 		model.WithBackendString(model.WhisperBackend),
