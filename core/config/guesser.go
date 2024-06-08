6f4c503a4d6677f1ca7a22fe0062ad7f0dee1de5
Ettore Di Giacinto <mudler@users.noreply.github.com>
2024-06-08 22:13:02 +0200

    feat(llama.cpp): guess model defaults from file (#2522)
    * wip: guess informations from gguf file
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * update go mod
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Small fixups
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Identify llama3
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Do not try to guess the name, as reading gguf files can be expensive
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Allow to disable guessing
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    ---------
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- core/config/guesser.go
+++ core/config/guesser.go
@@ -4,11 +4,28 @@ import (
 	"os"
 	"path/filepath"
 
-	gguf "github.com/gpustack/gguf-parser-go"
 	"github.com/rs/zerolog/log"
+
+	gguf "github.com/thxcode/gguf-parser-go"
 )
 
-func guessDefaultsFromFile(cfg *BackendConfig, modelPath string, defaultCtx int) {
+type familyType uint8
+
+const (
+	Unknown familyType = iota
+	LLaMa3             = iota
+	LLama2             = iota
+)
+
+var defaultsTemplate map[familyType]TemplateConfig = map[familyType]TemplateConfig{
+	LLaMa3: {
+		Chat:        "<|begin_of_text|>{{.Input }}\n<|start_header_id|>assistant<|end_header_id|>",
+		ChatMessage: "<|start_header_id|>{{ .RoleName }}<|end_header_id|>\n\n{{.Content }}<|eot_id|>",
+	},
+}
+
+func guessDefaultsFromFile(cfg *BackendConfig, modelPath string) {
+
 	if os.Getenv("LOCALAI_DISABLE_GUESSING") == "true" {
 		log.Debug().Msgf("guessDefaultsFromFile: %s", "guessing disabled with LOCALAI_DISABLE_GUESSING")
 		return
@@ -19,28 +36,52 @@ func guessDefaultsFromFile(cfg *BackendConfig, modelPath string, defaultCtx int)
 		return
 	}
 
-	// We try to guess only if we don't have a template defined already
-	guessPath := filepath.Join(modelPath, cfg.ModelFileName())
-
-	defer func() {
-		if r := recover(); r != nil {
-			log.Error().Msgf("guessDefaultsFromFile: %s", "panic while parsing gguf file")
-		}
-	}()
-
-	defer func() {
-		if cfg.ContextSize == nil {
-			if defaultCtx == 0 {
-				defaultCtx = defaultContextSize
-			}
-			cfg.ContextSize = &defaultCtx
-		}
-	}()
-
-	// try to parse the gguf file
-	f, err := gguf.ParseGGUFFile(guessPath)
-	if err == nil {
-		guessGGUFFromFile(cfg, f, defaultCtx)
+	if cfg.HasTemplate() {
+		// nothing to guess here
+		log.Debug().Any("name", cfg.Name).Msgf("guessDefaultsFromFile: %s", "template already set")
 		return
 	}
+
+	// We try to guess only if we don't have a template defined already
+	f, err := gguf.ParseGGUFFile(filepath.Join(modelPath, cfg.ModelFileName()))
+	if err != nil {
+		// Only valid for gguf files
+		log.Debug().Msgf("guessDefaultsFromFile: %s", "not a GGUF file")
+		return
+	}
+
+	log.Debug().
+		Any("eosTokenID", f.Tokenizer().EOSTokenID).
+		Any("bosTokenID", f.Tokenizer().BOSTokenID).
+		Any("modelName", f.Model().Name).
+		Any("architecture", f.Architecture().Architecture).Msgf("Model file loaded: %s", cfg.ModelFileName())
+
+	// guess the name
+	if cfg.Name == "" {
+		cfg.Name = f.Model().Name
+	}
+
+	family := identifyFamily(f)
+
+	if family == Unknown {
+		log.Debug().Msgf("guessDefaultsFromFile: %s", "family not identified")
+		return
+	}
+
+	templ, ok := defaultsTemplate[family]
+	if ok {
+		cfg.TemplateConfig = templ
+		log.Debug().Any("family", family).Msgf("guessDefaultsFromFile: guessed template %+v", cfg.TemplateConfig)
+	} else {
+		log.Debug().Any("family", family).Msgf("guessDefaultsFromFile: no template found for family")
+	}
+}
+
+func identifyFamily(f *gguf.GGUFFile) familyType {
+	switch {
+	case f.Architecture().Architecture == "llama" && f.Tokenizer().EOSTokenID == 128009:
+		return LLaMa3
+	}
+
+	return Unknown
 }
