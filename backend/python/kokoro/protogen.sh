fe4fc116e366eb652a8a4a0ffec1a5831b51add9
Ettore Di Giacinto <mudler@users.noreply.github.com>
2025-01-16 22:23:09 +0100

    feat(tts): Add Kokoro backend (#4616)
    * feat(kokoro): Add new TTS backend
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Add kokoro to images
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Support combined voices
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Ignore pt and onnx
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Add plbert and istfnet
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    ---------
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/python/kokoro/protogen.sh
+++ backend/python/kokoro/protogen.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-backend_dir=$(dirname $0)
-if [ -d $backend_dir/common ]; then
-    source $backend_dir/common/libbackend.sh
-else
-    source $backend_dir/../common/libbackend.sh
-fi
+source $(dirname $0)/../common/libbackend.sh
 
-python3 -m grpc_tools.protoc -I../.. -I./ --python_out=. --grpc_python_out=. backend.proto
+python3 -m grpc_tools.protoc -I../.. --python_out=. --grpc_python_out=. backend.proto
