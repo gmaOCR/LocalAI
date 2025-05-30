290f6f37aabcee830dcbaf64d4cd0e0c4d8b9553
Ettore Di Giacinto <mudler@users.noreply.github.com>
2025-05-30 10:52:55 +0200

    feat(chatterbox): add new backend (#5524)
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/python/chatterbox/run.sh
+++ backend/python/chatterbox/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-backend_dir=$(dirname $0)
-if [ -d $backend_dir/common ]; then
-    source $backend_dir/common/libbackend.sh
-else
-    source $backend_dir/../common/libbackend.sh
-fi
+source $(dirname $0)/../common/libbackend.sh
 
 startBackend $@
