37fa03792fffe56246ad28ee8e348e74835d5613
Ettore Di Giacinto <mudler@users.noreply.github.com>
2025-06-18 19:48:50 +0200

    chore(backends): move `bark-cpp` to the backend gallery (#5682)
    chore(bark-cpp): move outside from binary
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/Dockerfile.go
+++ backend/Dockerfile.go
@@ -123,9 +123,9 @@ EOT
 
 COPY . /LocalAI
 
-RUN cd /LocalAI && make backend-assets/grpc/${BACKEND}
+RUN cd /LocalAI && make backend-assets/grpc/bark-cpp
 
 FROM scratch
 
-COPY --from=builder /LocalAI/backend-assets/grpc/${BACKEND} ./
-COPY --from=builder /LocalAI/backend/go/${BACKEND}/run.sh ./
+COPY --from=builder /LocalAI/backend-assets/grpc/bark-cpp ./
+COPY --from=builder /LocalAI/backend/go/bark/run.sh ./
