4c1cf6be92717aa11bd4c3f72252e985fc8d4b74
Ettore Di Giacinto <mudler@users.noreply.github.com>
2025-06-24 17:08:27 +0200

    feat(backend gallery): add meta packages (#5696)
    * feat(backend gallery): add meta packages
    
    So we can have meta packages such as "vllm" that automatically installs
    the corresponding package depending on the GPU that is being currently
    detected in the system.
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * feat: use a metadata file
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    ---------
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- core/system/capabilities.go
+++ core/system/capabilities.go
@@ -1,7 +1,6 @@
 package system
 
 import (
-	"os"
 	"strings"
 
 	"github.com/mudler/LocalAI/pkg/xsysinfo"
@@ -10,29 +9,6 @@ import (
 
 type SystemState struct {
 	GPUVendor string
-}
-
-func (s *SystemState) Capability() string {
-	if os.Getenv("LOCALAI_FORCE_META_BACKEND_CAPABILITY") != "" {
-		return os.Getenv("LOCALAI_FORCE_META_BACKEND_CAPABILITY")
-	}
-
-	capabilityRunFile := "/run/localai/capability"
-	if os.Getenv("LOCALAI_FORCE_META_BACKEND_CAPABILITY_RUN_FILE") != "" {
-		capabilityRunFile = os.Getenv("LOCALAI_FORCE_META_BACKEND_CAPABILITY_RUN_FILE")
-	}
-
-	// Check if /run/localai/capability exists and use it
-	// This might be used by e.g. container images to specify which
-	// backends to pull in automatically when installing meta backends.
-	if _, err := os.Stat(capabilityRunFile); err == nil {
-		capability, err := os.ReadFile(capabilityRunFile)
-		if err == nil {
-			return string(capability)
-		}
-	}
-
-	return s.GPUVendor
 }
 
 func GetSystemState() (*SystemState, error) {
@@ -54,13 +30,13 @@ func detectGPUVendor() (string, error) {
 		if gpu.DeviceInfo != nil {
 			if gpu.DeviceInfo.Vendor != nil {
 				gpuVendorName := strings.ToUpper(gpu.DeviceInfo.Vendor.Name)
-				if strings.Contains(gpuVendorName, "NVIDIA") {
+				if gpuVendorName == "NVIDIA" {
 					return "nvidia", nil
 				}
-				if strings.Contains(gpuVendorName, "AMD") {
+				if gpuVendorName == "AMD" {
 					return "amd", nil
 				}
-				if strings.Contains(gpuVendorName, "INTEL") {
+				if gpuVendorName == "INTEL" {
 					return "intel", nil
 				}
 				return "nvidia", nil
